d9128f77522401d1aa8bccae2e4784a1a6594e7c
Davidhernandezn <d4vidhernandezn@gmail.com>
2021-11-30 19:39:15 -0600

    Agregamos el SplashScreen con el logotipo de la app

--- app/src/main/java/com/davidhernandezn/climapp/MainActivity.kt
+++ app/src/main/java/com/davidhernandezn/climapp/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Thread.sleep(2000)//no recomendado
+        setTheme(R.style.Theme_ClimApp)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
